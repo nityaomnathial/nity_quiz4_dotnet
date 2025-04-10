08696ac4c639d00258943be5d6b4d39431528e90
Nityaom Nathial <[emailprotected]>
2025-04-10 14:08:14 -0400

    Add database access message

---
+++ DatabaseAccess.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Quiz4App
+{
+    class DatabaseAccess
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Database access has already been done on April 1, 2025 by Nityaom Nathial");
+        }
+    }
+}
